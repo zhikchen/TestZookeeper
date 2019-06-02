44d4e63a892d6f2b4fade8f1f384783137025a9d
zhikchen <zhikchen@yeah.net>
2019-06-02 10:06:06 +0800

    创建zookeeper客户端

---
+++ src/cn/edu/jxufe/testZookeeper/demo/ZookeeperDemo.java
@@ -0,0 +1,32 @@
+package cn.edu.jxufe.testZookeeper.demo;
+
+import java.io.IOException;
+
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
+import org.junit.Test;
+
+public class ZookeeperDemo {
+
+	private String connectString = "master:2181,slave1:2181,slave2:2181";
+	private int sessionTimeout = 3000;
+	private ZooKeeper zooKeeperClient;
+
+	
+	@Test
+	//创建zookeeper客户端
+	public void init() throws IOException {
+		zooKeeperClient = new ZooKeeper(connectString , sessionTimeout , new Watcher() {
+			
+			@Override
+			public void process(WatchedEvent event) {
+				System.out.println(".....");
+				System.out.println(event.getPath());
+				System.out.println(event.getState());
+				System.out.println(event.getType());
+				System.out.println(event.getWrapper());
+			}
+		});
+	}
+}
